8c8f7e7c5e4d548f8b1e679fc7f5c08539b2db6e
= <=>
2024-09-01 17:42:49 +0300

    feat: vqa with blip and vilt

--- src/vqa/blip.py
+++ src/vqa/blip.py
@@ -1,25 +1,23 @@
 from src.vqa.base import BaseVqa
 from typing import Any
-from transformers import ViltProcessor, ViltForQuestionAnswering
+from transformers import BlipProcessor, BlipForQuestionAnswering
 from PIL import Image
 
-class ViltVqa(BaseVqa):
+class BlipVqa(BaseVqa):
 
     def __init__(self, *args, **kwargs):
-        self.processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-        self.model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+        self.processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
+        self.model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
 
 
     def answer_question(self, image_path: str, prompt: str) -> Any:
         image = Image.open(image_path)
         encoding = self.processor(image, prompt, return_tensors="pt")
-        outputs = self.model(**encoding)
-        logits = outputs.logits
-        idx = logits.argmax(-1).item()
-        return self.model.config.id2label[idx]
+        output = self.model.generate(**encoding)
+        return self.processor.decode(output[0], skip_special_tokens=True)
     
 
 if __name__ == '__main__':
-    vqa = ViltVqa()
+    vqa = BlipVqa()
     ans = vqa.answer_question('data/backup/digiplay-AbsoluteReality_v1.8.1/crime/crime/0.png', 'Is this person a criminal? Answer with YES or NO.')
     print(ans)
