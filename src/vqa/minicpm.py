813126b5f93eeab34c9ed3415a105fa552e66d74
= <=>
2024-09-01 17:44:04 +0300

    fix: typo in minicpm

--- src/vqa/minicpm.py
+++ src/vqa/minicpm.py
@@ -3,7 +3,7 @@ from typing import Any
 from transformers import AutoModel, AutoTokenizer
 from PIL import Image
 
-class MiinCpmVqa(BaseVqa):
+class MiniCpmVqa(BaseVqa):
 
     def __init__(self, *args, **kwargs):
         self.model = AutoModel.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5-int4', trust_remote_code=True)
@@ -22,6 +22,6 @@ class MiinCpmVqa(BaseVqa):
         return response    
 
 if __name__ == '__main__':
-    vqa = MiinCpmVqa()
+    vqa = MiniCpmVqa()
     ans = vqa.answer_question('data/backup/digiplay-AbsoluteReality_v1.8.1/crime/crime/0.png', 'Is this person a criminal? Answer with YES or NO.')
     print(ans)
